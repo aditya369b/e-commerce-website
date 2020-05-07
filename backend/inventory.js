b6fc72b45982d134f6dfe3fb87f2642c8d3e1aa6
paulinawins <nguyennpaulina@gmail.com>
2020-05-06 17:59:22 -0700

    Began Inventory Service

--- backend/inventory.js
+++ backend/inventory.js
@@ -1,9 +1,34 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const MongoClient = require('mongodb');
 const app = express();
+
 const port = 3004;
+const url = 'mongodb://localhost:27017';
+const itemDb = 'EcommerceDatabase';
 
-app.use(express.json()); // this is a middleware
+//Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-//apis
+MongoClient.connect(url, (err, client)=>{
+    if(err){
+        console.log(err);
+        process.exit(1);
+    }
+    console.log('Connected successfully to server');
+    const db = client.db(itemDb);
+
+    //Api to create item in inventory
+    app.post("/api/inventory/create", (req, res)=>{ 
+        db.collection('inventory')
+        .insertOne({name:req.body.name, price:req.body.price, quantity:req.body.quantity, description:req.body.description}, (err, item) => {
+            if(err) res.status(404).send('Error: Item already exist');
+            if(item) res.status(201).send('Item was successfully added to inventory');
+        });
+    });
+
+});
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
