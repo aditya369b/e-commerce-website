48fc173bf424a1287bbe87b35b8ef997f610bd89
bittersweetsalt <38972085+bittersweetsalt@users.noreply.github.com>
2020-05-10 15:18:00 -0700

    Seller Page changes (includes updated inventory.js)
    Seller page was modified (from Sanjay's Branch) from class object structure -> React Hook structure to better integrate with Redux.
    inventory.js was set to recent (updated since 5/9/20)
    [Todo]
    integrate with Redux.

--- backend/inventory.js
+++ backend/inventory.js
@@ -1,51 +1,146 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const MongoClient = require('mongodb');
 const app = express();
-const cors = require('cors');
-
 const port = 3004;
-const url = 'mongodb://localhost:27017';
-const itemDb = 'EcommerceDatabase';
 
-//Middleware
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+/** Database */
+const { MongoClient, ObjectID } = require("mongodb");
+const url = "mongodb://localhost:27017";
+const dbName = "MochaDatabase";
+const client = new MongoClient(url);
 
-app.use(cors());
+app.use(express.json()); // this is a middleware
 
-MongoClient.connect(url, (err, client)=>{
-    if(err){
-        console.log(err);
-        process.exit(1);
-    }
-    console.log('Connected successfully to server');
-    const db = client.db(itemDb);
+//apis
+/*
 
-    //Api
-    //create item in inventory
-    app.post("/api/inventory/create", (req, res)=>{ 
-        db.collection('inventory')
-        .insertOne({name:req.body.name, price:req.body.price, quantity:req.body.quantity, description:req.body.description}, (err, item) => {
-            if(err) res.status(404).send('Error: Item already exist');
-            if(item) res.status(201).send('Item was successfully added to inventory');
+"/api/item/create" - To add an item in db
+"/api/item/edit" - To edit an item
+"/api/item/delete" - To modify an item as unavailable
+"/api/item/getInfo" - To fetch item details for a buyer/seller
+
+*/
+client.connect((err) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+
+  app.post("/api/item/create", (req, res) => {
+      // Should check whether itemId exisits in db or not. May do it at front end as well ????
+    db.collection("ItemCollection")
+      .insert({
+        itemId: req.body.username + "_" + req.body.name, // unique id
+        itemDetails: {
+          itemName: req.body.name,
+          itemPrice: req.body.price,
+          itemDesc: req.body.desc,
+          itemDate: req.body.date,
+          itemURL: req.body.URL,
+        },
+        seller: req.body.username,
+        salesCount: 0,
+        forSale: true,
+      })
+      .then((doc) => {
+        console.log(doc);
+        res.send({
+          valid: true,
+          result: doc,
         });
-    });
+      })
+      .catch((e) => {
+        console.log(e);
+        res.send("Error", e);
+      });
 
-    //get item info from inventory
-    //testing purposes
-    app.get("/api/inventory/getItem", (req, res)=>{ //temporarily changed to get
-        db.collection('EcommerceDatabase')
-        .find({name: req.query.name}).toArray()     //temporarily changed to req.query for testing purposes
-        .then(obj=>{
-            res.send(obj);
-        })
-        .catch( e=> {
-            res.status(404).send('item not found');
+    db.collection("UserCollection")
+      .findOneAndUpdate(
+        {
+          userId: req.body.username,
+        },
+        {
+          $push: { items: req.body.username + "_" + req.body.itemName },
+        }
+      )
+      .then((doc) => {
+        console.log(doc);
+        res.send({ valid: doc });
+      })
+      .catch((e) => {
+        console.log(e);
+        res.send("Error ", e);
+      });
+  });
+
+  app.post("/api/item/delete", (req, res) => {
+    db.collection("ItemCollection")
+      .findOneAndUpdate(
+        {
+          itemId: req.body.username + "_" + req.body.itemName,
+        },
+        {
+          $set: { forSale: false },
+        }
+      )
+      .then((doc) => {
+        console.log(doc);
+        res.send({ valid: doc });
+      })
+      .catch((e) => {
+        console.log(e);
+        res.send("Error ", e);
+      });
+  });
+
+  app.post("/api/item/edit", (req, res) => {
+    db.collection("ItemCollection")
+      .findOneAndUpdate(
+        {
+          itemName: req.body.itemId,
+        },
+        {
+          //   $set: { itemName: false, itemPrice: 99, itemDesc },
+          $set: req.body.params,
+        }
+      )
+      .then((doc) => {
+        console.log(doc);
+        res.send({ valid: doc });
+      })
+      .catch((e) => {
+        console.log(e);
+        res.send("Error ", e);
+      });
+  });
+
+  app.post("/api/item/getInfo", (req, res) => {
+    let itemDetails = [];
+
+    db.collection("UserCollection")
+      .aggregate([
+        { $lookup:
+           {
+             from: 'ItemCollection',
+             localField: 'items',
+             foreignField: 'itemId',
+             as: 'itemDetails'
+           }
+         }
+        ]).toArray(function(err, response) {
+        if (err) throw err;
+        console.log(JSON.stringify(response));
+        for(let i=0; i<response.length; i++){
+            let resObj = response[i];
+            if(resObj["userId"] == req.body.username)
+            {   itemDetails = resObj["itemDetails"];
+                break;}
+        }
+        res.send({result : itemDetails});
         });
-    });
+  });
 
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 });
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
