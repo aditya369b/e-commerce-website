347ed0ea987a4fe6cd4b1bb7944cbad670330bab
aditya369b <aditya369b@gmail.com>
2020-05-11 20:08:16 -0700

    Implemented wss for transaction (news feed) and items view count

--- backend/notification.js
+++ backend/notification.js
@@ -2,13 +2,14 @@ const WebSocket = require('ws');
 const options = {port: 3002,};
 const redis = require('redis');
 const client = redis.createClient();
+const client_transaction = redis.createClient();
 //apis
 
 const wss = new WebSocket.Server(options);
 
-client.subscribe('transactionChannel');
+client_transaction.subscribe('transactionChannel');
 
-client.on('message', (channel, message) => {
+client_transaction.on('message', (channel, message) => {
     console.log("Message: ",message);
     broadcastMessage(message);
 });
@@ -22,33 +23,57 @@ const broadcastMessage = (message) => {
     };
 
 const updateItemViewerCount = (itemId, itemCount) => {
-    broadcastMessage({
+    broadcastMessage(JSON.stringify({
         type: 'UPDATE_VIEW_COUNT',
         id: itemId,
         count: itemCount,
-    })
+    }))
 }
 
 wss.on('connection' , (ws) => {
-
+    
     ws.on('message', (message) => {
         const msgObj = JSON.parse(message);
-        let itemCount = client.get(msgObj.id);
+        ws.id = msgObj.id;
+        // client.incr(msgObj.id);
+        let itemCount;
+
+        client.incr(msgObj.id, (err, cachedValue) => {
+
+            if(err) {
+              console.log(err);
+            }
+            console.log(cachedValue);
+            itemCount = cachedValue;
+        
+        console.log(itemCount);
         switch(msgObj.type){
             case 'CONNECT':
-                client.incr(msgObj.id);
-                updateItemViewerCount(msgObj.id, itemCount+1);
+                // client.incr(msgObj.id);
+                updateItemViewerCount(msgObj.id, itemCount);
                 break;
             case 'DISCONNECT':
-                client.decr(msgObj.id);
+                // client.decr(msgObj.id);
                 updateItemViewerCount(msgObj.id, itemCount-1);
                 break;
             default:
                 console.log('Message Type not Supported');
         }
+
+        });
     });
 
-    ws.on('close', () => {});
+    ws.on('close', (e) => {
+        client.decr(ws.id, (err, cachedValue) => {
+
+            if(err) {
+              console.log(err);
+            }
+            console.log(cachedValue);
+        updateItemViewerCount(ws.id, cachedValue);
+        });
+
+    });
 
     ws.on('error', (e) => {});
 
