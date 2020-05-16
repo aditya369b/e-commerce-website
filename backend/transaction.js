3afac13f74fcfc36004d64b89c0b3aaac415a7ba
aditya369b <aditya369b@gmail.com>
2020-05-16 01:42:41 -0700

    Added API for transaction

--- backend/transaction.js
+++ backend/transaction.js
@@ -2,8 +2,83 @@ const express = require("express");
 const app = express();
 const port = 3003;
 
+const KafkaProducer = require('./Kafka/KafkaProducer.js');
+
+const producer = new KafkaProducer('myTopic');
+
 app.use(express.json()); // this is a middleware
 
 //apis
+
+app.post('/transaction', (req,res) => {
+
+    let username = req.body.username;
+    let items = req.body.items;
+    let price = req.body.price;
+  
+    let date = new Date();
+    // let transactionId = _id;
+        // records the transaction
+    db.collection("transactionCollection")
+        .insert({
+          items: items,
+          buyer: username,
+          price: price,
+          purchaseDate: date,
+        })
+        .then((doc) => {
+          res.send({valid: true, result: doc,});
+        })
+        .catch((e) => {
+          console.log(e);
+          res.send("Error", e);
+        });
+  
+          /** Updates the buyer user in purchase history */
+        db.collection("UserCollection")
+        .findOneAndUpdate(
+          {
+            userId: username,
+          },
+          {
+            $push: { items: items },  // yet to verify
+          }
+        )
+        .then((doc) => {
+          console.log(doc);
+          res.send({ valid: doc });
+        })
+        .catch((e) => {
+          console.log(e);
+          res.send("Error ", e);
+        });
+  
+            /** Item's salescount is increased */
+        db.collection("ItemCollection")
+        .findOneAndUpdate(
+          {
+            itemId: items,
+          },
+          {
+            $inc: { salesCount: 1 },
+          }
+        )
+        .then((doc) => {
+          console.log(doc);
+          res.send({ valid: doc });
+        })
+        .catch((e) => {
+          console.log(e);
+          res.send("Error ", e);
+        });
+  
+
+
+
+    console.log('Pushing new item to queue');
+    producer.send(req.body);
+    res.send('Item added to queue');
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
